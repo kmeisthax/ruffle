fa97698c7831bff543c1e6b794190f65c758fb7e
Mike Welsh <mwelsh@gmail.com>
2019-06-29 02:17:49 -0700

    work

--- core/src/shape_utils.rs
+++ core/src/shape_utils.rs
@@ -165,7 +165,7 @@ impl<'a, T> ActivePaths<'a, T> {
     }
 }
 
-pub struct DrawShapeIter<'a> {
+pub struct ShapeConverter<'a> {
     // SWF shape commands.
     iter: std::slice::Iter<'a, swf::ShapeRecord>,
 
@@ -189,80 +189,93 @@ pub struct DrawShapeIter<'a> {
     // and for each new layer.
     fills: ActivePaths<'a, FillStyle>,
     strokes: ActivePaths<'a, LineStyle>,
+
+    // Output.
+    commands: Vec<DrawCommand<'a>>,
 }
 
-fn swf_shape_to_draws(shape: &swf::Shape) -> impl Iterator<Item = DrawCommand> {
-    let mut state = DrawShapeIter {
-        iter: shape.shape.iter(),
+impl<'a> ShapeConverter<'a> {
+    const DEFAULT_CAPACITY: usize = 512;
+    
+    pub fn from_shape(shape: &'a swf::Shape) -> Self {
+        ShapeConverter {
+            iter: shape.shape.iter(),
 
-        x: Twips::new(0),
-        y: Twips::new(0),
+            x: Twips::new(0),
+            y: Twips::new(0),
 
-        fill_styles: &shape.styles.fill_styles,
-        line_styles: &shape.styles.line_styles,
+            fill_styles: &shape.styles.fill_styles,
+            line_styles: &shape.styles.line_styles,
 
-        fill_style0: None,
-        fill_style1: None,
-        line_style: None,
+            fill_style0: None,
+            fill_style1: None,
+            line_style: None,
 
-        fills: ActivePaths::new(),
-        strokes: ActivePaths::new(),
-    };
+            fills: ActivePaths::new(),
+            strokes: ActivePaths::new(),
 
-    fn visit_edge(state: &mut DrawShapeIter, edge: Edge) {
-        state.fill_style0.as_mut().map(|path| path.add_edge(edge));
-        state.fill_style1.as_mut().map(|path| path.add_edge(edge));
-        state.line_style.as_mut().map(|path| path.add_edge(edge));
+            commands: Vec::with_capacity(Self::DEFAULT_CAPACITY),
+        }
     }
 
-    fn flush_paths(state: &mut DrawShapeIter) {}
-
-    fn flush_layer(state: &mut DrawShapeIter) {}
-
-    std::iter::from_fn(move || {
-        if let Some(record) = state.iter.next() {
+    pub fn into_commands(mut self) -> Vec<DrawCommand<'a>> {
+        while let Some(record) = self.iter.next() {
             match record {
                 ShapeRecord::StyleChange(style_change) => {
                     if let Some(ref styles) = style_change.new_styles {
-                        flush_layer(&mut state);
-                        state.fill_styles = &styles.fill_styles[..];
-                        state.line_styles = &styles.line_styles[..];
-                    }
-
-                    if let Some((dx, dy)) = style_change.move_to {
-                        state.x += dx;
-                        state.y += dy;
-                        // We've lifted the pen, so we're starting a new path.
-                        // Flush the previous path.
-                        flush_paths(&mut state);
+                        self.flush_layer();
+                        self.fill_styles = &styles.fill_styles[..];
+                        self.line_styles = &styles.line_styles[..];
                     }
 
                     if let Some(fs) = style_change.fill_style_1 {
-                        let fill_style = &state.fill_styles[fs as usize - 1];
-                        let new_path = if fs != 0 { Some(Path::new(fill_style, NonZeroU32::new(fs).unwrap()) } else { None };
-                        if let Some(path) = state.fill_style1.replace(new_path) {
-                            state.fills.merge_path(path);
+                        if let Some(path) = self.fill_style1.take() {
+                            self.fills.merge_path(path);
+                        }
+
+                        let fs_index = NonZeroU32::new(fs).unwrap();
+                        self.fill_style1 = if fs != 0 {
+                            let fill_style = &self.fill_styles[fs as usize - 1];
+                            Some(Path::new(fill_style, fs_index))
+                        } else {
+                            None
                         }
                     }
 
                     if let Some(fs) = style_change.fill_style_0 {
+                        if let Some(path) = self.fill_style0.take() {
+                            self.fills.merge_path(path);
+                        }
 
+                        let fs_index = NonZeroU32::new(fs).unwrap();
+                        self.fill_style0 = if fs != 0 {
+                            let fill_style = &self.fill_styles[fs as usize - 1];
+                            Some(Path::new(fill_style, fs_index))
+                        } else {
+                            None
+                        }
                     }
 
-                    if let Some(ls) = style_change.line_style {
+                    if let Some(ls) = style_change.line_style {}
 
+                    if let Some((dx, dy)) = style_change.move_to {
+                        self.x += dx;
+                        self.y += dy;
+                        // We've lifted the pen, so we're starting a new path.
+                        // Flush the previous path.
+                        self.flush_paths();
                     }
                 }
 
                 ShapeRecord::StraightEdge { delta_x, delta_y } => {
-                    state.x += *delta_x;
-                    state.y += *delta_y;
+                    self.x += *delta_x;
+                    self.y += *delta_y;
                     let edge = Edge::LineTo {
-                        x: state.x,
-                        y: state.y,
+                        x: self.x,
+                        y: self.y,
                     };
 
-                    visit_edge(&mut state, edge);
+                    self.visit_edge(edge);
                 }
 
                 ShapeRecord::CurvedEdge {
@@ -271,19 +284,41 @@ fn swf_shape_to_draws(shape: &swf::Shape) -> impl Iterator<Item = DrawCommand> {
                     anchor_delta_x,
                     anchor_delta_y,
                 } => {
-                    let x1 = state.x + *control_delta_x;
-                    let y1 = state.y + *control_delta_y;
+                    let x1 = self.x + *control_delta_x;
+                    let y1 = self.y + *control_delta_y;
                     let x2 = x1 + *anchor_delta_x;
                     let y2 = y1 + *anchor_delta_y;
-                    state.x = x2;
-                    state.y = y2;
+                    self.x = x2;
+                    self.y = y2;
                     let edge = Edge::CurveTo { x1, y1, x2, y2 };
 
-                    visit_edge(&mut state, edge);
+                    self.visit_edge(edge);
                 }
             }
         }
+        
+        self.commands
+    }
+    
+    /// Adds an edge to the current path for the active fills/strokes.
+    fn visit_edge(&mut self, edge: Edge) {
+        self.fill_style0.as_mut().map(|path| path.add_edge(edge));
+        self.fill_style1.as_mut().map(|path| path.add_edge(edge));
+        self.line_style.as_mut().map(|path| path.add_edge(edge));
+    }
 
-        None
-    })
+    /// When the pen jumps to a new position, we reset the active path.
+    fn flush_paths(&mut self) {
+    }
+
+    /// When a new layer starts, all paths are flushed and turned into drawing commands.
+    fn flush_layer(self: &mut Self) {        
+        let fills = std::mem::replace(&mut self.fills.0, vec![]);
+        let strokes = std::mem::replace(&mut self.strokes.0, vec![]);
+
+        // Draw fills, and then strokes.
+        // Strokes in the same layer always appear on top of fills.
+        self.commands.extend(fills.into_iter().map(|fill| DrawCommand::Fill(fill)));
+        self.commands.extend(strokes.into_iter().map(|stroke| DrawCommand::Stroke(stroke)));
+    }
 }
