94da2d7448e76ec3ed01f9fb4a73d62df68a6c01
Nathan Adams <dinnerbone@dinnerbone.com>
2019-12-18 22:25:54 +0100

    core: Changed KeyCode into an enum that maps out every Flash key code

--- core/src/display_object/button.rs
+++ core/src/display_object/button.rs
@@ -1,9 +1,10 @@
 use crate::context::{ActionType, RenderContext, UpdateContext};
 use crate::display_object::{DisplayObjectBase, TDisplayObject};
-use crate::events::ButtonEvent;
+use crate::events::{ButtonEvent, KeyCode};
 use crate::prelude::*;
 use gc_arena::{Collect, GcCell, MutationContext};
 use std::collections::BTreeMap;
+use std::convert::TryFrom;
 
 #[derive(Clone, Debug, Collect, Copy)]
 #[collect(no_drop)]
@@ -37,7 +38,7 @@ impl<'gc> Button<'gc> {
                 let button_action = ButtonAction {
                     action_data: action_data.clone(),
                     condition: *condition,
-                    key_code: action.key_code,
+                    key_code: action.key_code.and_then(|k| KeyCode::try_from(k).ok()),
                 };
                 actions.push(button_action);
             }
@@ -273,7 +274,7 @@ impl<'gc> ButtonData<'gc> {
         &mut self,
         context: &mut UpdateContext<'_, 'gc, '_>,
         condition: swf::ButtonActionCondition,
-        key_code: Option<u8>,
+        key_code: Option<KeyCode>,
     ) {
         if let Some(parent) = self.base.parent {
             for action in &self.static_data.read().actions {
@@ -318,7 +319,7 @@ enum ButtonState {
 struct ButtonAction {
     action_data: crate::tag_utils::SwfSlice,
     condition: swf::ButtonActionCondition,
-    key_code: Option<u8>,
+    key_code: Option<KeyCode>,
 }
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
