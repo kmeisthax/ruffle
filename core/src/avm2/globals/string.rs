6f103f18bd6e0cf070cd268991d8176361640a51
David Wendt <dcrkid@yahoo.com>
2020-09-03 22:22:23 -0400

    avm2: Implement `String.length`.

--- core/src/avm2/globals/string.rs
+++ core/src/avm2/globals/string.rs
@@ -5,6 +5,7 @@ use crate::avm2::class::Class;
 use crate::avm2::method::Method;
 use crate::avm2::names::{Namespace, QName};
 use crate::avm2::object::Object;
+use crate::avm2::traits::Trait;
 use crate::avm2::value::Value;
 use crate::avm2::Error;
 use gc_arena::{GcCell, MutationContext};
@@ -27,13 +28,35 @@ pub fn class_init<'gc>(
     Ok(Value::Undefined)
 }
 
+/// Implements `String.length`
+pub fn length<'gc>(
+    activation: &mut Activation<'_, 'gc, '_>,
+    this: Option<Object<'gc>>,
+    _args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error> {
+    if let Some(this) = this {
+        return Ok(Value::from(this).coerce_to_string(activation)?.len().into());
+    }
+
+    Ok(Value::Undefined)
+}
+
 /// Construct `String`'s class.
 pub fn create_class<'gc>(mc: MutationContext<'gc, '_>) -> GcCell<'gc, Class<'gc>> {
-    Class::new(
+    let class = Class::new(
         QName::new(Namespace::package(""), "String"),
         Some(QName::new(Namespace::public_namespace(), "Object").into()),
         Method::from_builtin(instance_init),
         Method::from_builtin(class_init),
         mc,
-    )
+    );
+
+    let mut write = class.write(mc);
+
+    write.define_instance_trait(Trait::from_getter(
+        QName::new(Namespace::public_namespace(), "length"),
+        Method::from_builtin(length),
+    ));
+
+    class
 }
