f8dabaabd3caa7ec30a1805e20ff441c8cd17bb2
David Wendt <dcrkid@yahoo.com>
2021-03-17 21:53:03 -0400

    avm2: Impl `Vector.length`

--- core/src/avm2/globals/vector.rs
+++ core/src/avm2/globals/vector.rs
@@ -7,6 +7,7 @@ use crate::avm2::method::Method;
 use crate::avm2::names::{Namespace, QName};
 use crate::avm2::object::{Object, TObject, VectorObject};
 use crate::avm2::scope::Scope;
+use crate::avm2::traits::Trait;
 use crate::avm2::value::Value;
 use crate::avm2::Error;
 use gc_arena::{GcCell, MutationContext};
@@ -49,6 +50,42 @@ pub fn class_init<'gc>(
     Ok(Value::Undefined)
 }
 
+/// `Vector.length` getter
+pub fn length<'gc>(
+    _activation: &mut Activation<'_, 'gc, '_>,
+    this: Option<Object<'gc>>,
+    _args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error> {
+    if let Some(this) = this {
+        if let Some(vector) = this.as_vector_storage() {
+            return Ok(vector.length().into());
+        }
+    }
+
+    Ok(Value::Undefined)
+}
+
+/// `Vector.length` setter
+pub fn set_length<'gc>(
+    activation: &mut Activation<'_, 'gc, '_>,
+    this: Option<Object<'gc>>,
+    args: &[Value<'gc>],
+) -> Result<Value<'gc>, Error> {
+    if let Some(this) = this {
+        if let Some(mut vector) = this.as_vector_storage_mut(activation.context.gc_context) {
+            let new_length = args
+                .get(0)
+                .cloned()
+                .unwrap_or(Value::Unsigned(0))
+                .coerce_to_u32(activation)? as usize;
+
+            vector.resize(new_length)?;
+        }
+    }
+
+    Ok(Value::Undefined)
+}
+
 /// Vector deriver
 pub fn vector_deriver<'gc>(
     base_proto: Object<'gc>,
@@ -72,6 +109,15 @@ pub fn create_class<'gc>(mc: MutationContext<'gc, '_>) -> GcCell<'gc, Class<'gc>
     let mut write = class.write(mc);
 
     write.set_attributes(ClassAttributes::GENERIC);
+
+    write.define_instance_trait(Trait::from_getter(
+        QName::new(Namespace::public(), "length"),
+        Method::from_builtin(length),
+    ));
+    write.define_instance_trait(Trait::from_setter(
+        QName::new(Namespace::public(), "length"),
+        Method::from_builtin(set_length),
+    ));
 
     class
 }
