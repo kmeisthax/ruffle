027a25b061510909bd39d06e2f9f603a98de91f7
Adrian Wielgosik <adrian.wielgosik@gmail.com>
2021-05-31 18:05:45 +0200

    avm2: `Array.every/some` should early return

--- core/src/avm2/globals/array.rs
+++ core/src/avm2/globals/array.rs
@@ -457,13 +457,12 @@ pub fn every<'gc>(
             .unwrap_or(Value::Null)
             .coerce_to_object(activation)
             .ok();
-        let mut is_every = true;
         let mut iter = ArrayIter::new(activation, this)?;
 
         while let Some(r) = iter.next(activation) {
             let (i, item) = r?;
 
-            is_every &= callback
+            let result = callback
                 .call(
                     receiver,
                     &[item, i.into(), this.into()],
@@ -471,9 +470,13 @@ pub fn every<'gc>(
                     receiver.and_then(|r| r.proto()),
                 )?
                 .coerce_to_boolean();
+
+            if !result {
+                return Ok(false.into());
+            }
         }
 
-        return Ok(is_every.into());
+        return Ok(true.into());
     }
 
     Ok(Value::Undefined)
@@ -497,13 +500,12 @@ pub fn some<'gc>(
             .unwrap_or(Value::Null)
             .coerce_to_object(activation)
             .ok();
-        let mut is_some = false;
         let mut iter = ArrayIter::new(activation, this)?;
 
         while let Some(r) = iter.next(activation) {
             let (i, item) = r?;
 
-            is_some |= callback
+            let result = callback
                 .call(
                     receiver,
                     &[item, i.into(), this.into()],
@@ -511,9 +513,13 @@ pub fn some<'gc>(
                     receiver.and_then(|r| r.proto()),
                 )?
                 .coerce_to_boolean();
+
+            if result {
+                return Ok(true.into());
+            }
         }
 
-        return Ok(is_some.into());
+        return Ok(false.into());
     }
 
     Ok(Value::Undefined)
